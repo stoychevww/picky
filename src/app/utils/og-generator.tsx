7496f15edce923b73f8388465af8807302704109
stoychevww <ytpremiumgogo22@gmail.com>
2025-02-14 00:52:56 +0200

    og-gen fix

--- src/app/utils/og-generator.tsx
+++ src/app/utils/og-generator.tsx
@@ -1,77 +1,77 @@
 import { ImageResponse } from "next/og";
 
-// Image metadata
 export const size = {
   width: 1200,
   height: 630,
 };
 
-// Image generation
-export async function GenerateImage(params: {
+export async function GenerateImage({
+  title,
+  description,
+}: {
   title: string;
   description?: string;
 }) {
-  // Fonts
-  const interSemiBold = fetch(
-    new URL("../fonts/Inter-SemiBold.ttf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  const interLight = fetch(
-    new URL("../fonts/Inter-Light.ttf", import.meta.url)
+  // Use WOFF2 format instead of TTF for better compression
+  const font = await fetch(
+    new URL(
+      "https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2",
+    ),
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 160,
           background: "black",
           width: "100%",
           height: "100%",
           display: "flex",
-          textAlign: "center",
           alignItems: "center",
           justifyContent: "center",
           color: "white",
-          fontFamily: "Inter, sans-serif",
+          fontFamily: "Inter",
         }}
       >
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            textAlign: "center",
             alignItems: "center",
-            justifyContent: "center",
-            gap: 4,
+            gap: "4px",
           }}
         >
-          <span style={{ fontWeight: 600 }}>{params.title}</span>
-          <span style={{ fontSize: 40, fontWeight: 300 }}>
-            {params.description}
-          </span>
+          <div
+            style={{
+              fontSize: "160px",
+              lineHeight: 1.2,
+            }}
+          >
+            {title}
+          </div>
+          {description && (
+            <div
+              style={{
+                fontSize: "40px",
+                opacity: 0.9,
+              }}
+            >
+              {description}
+            </div>
+          )}
         </div>
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
       ...size,
       fonts: [
         {
           name: "Inter",
-          data: await interSemiBold,
+          data: font,
           style: "normal",
-          weight: 600,
-        },
-        {
-          name: "Inter",
-          data: await interLight,
-          style: "normal",
-          weight: 300,
+          weight: 400,
         },
       ],
-    }
+    },
   );
 }
